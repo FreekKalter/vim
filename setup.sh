9ab6fa5bdb12db220cd051665b59d8c7fdefd831
Freek Kalter <freek@kalteronline.org>
2014-04-19 16:26:02 +0200

    make the setup script a bit more verbose

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ln -fs ~/.vim/.vimrc ~/.vimrc
+echo "[+] link created"
 mkdir -p bundle
+echo "[+] bundle dir created"
+echo "[-] getting vundle"
 git clone https://github.com/gmarik/vundle.git bundle/vundle
+echo "[+] vundle cloned"
+echo "[-] installing plugins"
 vim +PluginInstall +qall
+echo "[-] plugins installed"
